ecc4d0bdd41a4fa2d5f571b81cd4083f4e070808
Helen <helen@mobify.com>
2017-02-12 23:13:07 -0800

    add comments

--- react-app/src/utils/form-validation.js
+++ react-app/src/utils/form-validation.js
@@ -1,16 +1,32 @@
-export const required = (value, allValue, props) => value ? undefined : props.intl.messages['formValidation.required']
+/**
+ * Form field validation functions
+ */
 
-const minValue = min => (value, allValue, props) => value && value.length < min ? props.intl.messages['formValidation.minLength'].replace('${min}', min) : undefined
+// Required validation
+export const required = (value, allValue, props) =>
+	value ? undefined : props.intl.messages['formValidation.required']
+
+// Min length validation
+// min 		pass in the minimum number
+const minValue = min => (value, allValue, props) =>
+	value && value.length < min ?
+	props.intl.messages['formValidation.minLength'].replace('${min}', min) : undefined
 export const minValue8 = minValue(8)
 
-const characterExist = (regex, errMsg) => (value, allValue, props) => !regex.test(value) ? props.intl.messages[errMsg] : undefined
+// Charater type exist validation
+const characterExist = (regex, errMsg) => (value, allValue, props) =>
+	!regex.test(value) ? props.intl.messages[errMsg] : undefined
 export const haveUppercase = characterExist(/[A-Z]+/, 'formValidation.uppercase')
 export const haveLowercase = characterExist(/[a-z]+/, 'formValidation.lowercase')
 export const haveNumber = characterExist(/[0-9]+/, 'formValidation.number')
 export const haveSpecial = characterExist(/[$@$!%*?&]+/, 'formValidation.special')
 
-export const matchField = (field) => (value, allValue, props) => allValue[field] !== value ? props.intl.messages['formValidation.passwordMatch'] : undefined
+// Field match validation
+// field 	pass in the form field name you want this field to match against
+export const matchField = (field) => (value, allValue, props) =>
+	allValue[field] !== value ? props.intl.messages['formValidation.passwordMatch'] : undefined
 
+// Email validation
 export const email = (value, allValue, props) =>
   value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ?
   props.intl.messages['formValidation.email'] : undefined
